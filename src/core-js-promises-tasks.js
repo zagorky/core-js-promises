33a18895c36e11101af94902c7841f8e1b0f4314
Daria <156232667+zagorky@users.noreply.github.com>
2025-03-20 12:57:14 +0000

    feat: edit 4th solution abd add 5 solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -79,9 +79,7 @@ function getFirstResolvedPromiseResult(promises) {
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
 function getFirstPromiseResult(promises) {
-  return Promise.race(promises)
-    .then((res) => res)
-    .catch((error) => error);
+  return Promise.race(promises).then((res) => res);
 }
 
 /**
@@ -95,8 +93,8 @@ function getFirstPromiseResult(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises);
 }
 
 /**
