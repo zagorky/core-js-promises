5222c5b7803ccf02c1b5f7f5584f9477bb3b9ae9
Daria <156232667+zagorky@users.noreply.github.com>
2025-03-20 11:14:17 +0000

    feat:  add 3 solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -55,8 +55,8 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return Promise.race(promises);
 }
 
 /**
